8c367141434264ed2339b741293023a7dc42bfa3
Ryan Andrews <rcandrews44@gmail.com>
2021-04-12 14:29:31 -0400

    Started working on the forEach and fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+let breedButton = document.querySelector['#dog-image-container']
+let breedsUl = document.querySelector['#dog-breeds']
+
+message.forEach(function(){
+    '#dog-image-container'.append
+})
+
+breedButton.addEventListener('click', (evt) => {
+    
+    //  fetch("https://dog.ceo/api/breeds/image/random/4")
+//     .then(res => res.json())
+//     .then((responsePOJO) => {
+//         let blankLi = document.createElement("li")
+//         let blankImg = document.createElement('img')
+
+//         blankImg.src = responsePOJO.message
+
+//         blankLi.append(blankImg)
+//         breedsUl.append(blankLi)
+// })
+ })
